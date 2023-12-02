b388c9f656e2e2ef9f79b0d3e601c644f1b11736
Sergey Melekhin <sergey@melekhin.me>
2023-12-02 15:44:55 +0700

    day 2

---
+++ 02/1.py
@@ -0,0 +1,39 @@
+import re
+
+re = re.compile(r"(\d+)\s+(\w+)")
+
+
+def parse(l):
+    game = []
+    i = l.find(":")
+    if i == -1:
+        return None
+    l = l[i+1:]
+    sets = l.split(";")
+    for s in sets:
+        theset = {"red":0, "blue":0, "green":0}
+        cubes = s.split(",")
+        for c in cubes:
+            m = re.match(c.strip())
+            if m is None:
+                raise ValueError(f"Invalid cube: '{c}'")
+            theset[m.group(2)] = int(m.group(1))
+        game.append(theset)
+    return game
+
+
+with open("input.txt", "rt") as fi:
+    lines=fi.read().splitlines()
+    smm = 0
+    for n, l in enumerate(lines):
+        g = parse(l)
+        print(l)
+        print(g)
+        m = {"red":0, "blue":0, "green":0}
+        for s in g:
+            for k, v in s.items():
+                if m[k] < v:
+                    m[k] = v
+        if m["red"] <= 12 and m["blue"] <= 14 and m["green"] <= 13:
+            smm += n + 1
+    print(smm)
